dcba60a38245bf1ec2cd6a6d86874045490240f1
Nivaan Chakraborty <nivaanchakraborty.stu@npsinternational.edu.sg>
2026-06-09 09:58:48 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    }
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendNumber(1)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(0)
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
